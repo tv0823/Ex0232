d68d9b96ccf40955ae9ff795880ceba3d855ca49
tv0823 <tv0823@bs.amalnet.k12.il>
2024-10-08 13:59:23 +0300

    Initial commit

---
+++ src/Vehicles.java
@@ -0,0 +1,9 @@
+public class Vehicles{
+    private int carId;
+    private int carAge;
+    private int wheelsNum;
+    private boolean steeringWay; //true - wheel | false - kidon
+    private double pEmPerMinute;
+
+    
+}
